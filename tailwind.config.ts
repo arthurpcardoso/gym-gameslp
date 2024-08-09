349cb84cf8c51bd8fe3d3d1ddc1e9eb9539b50ce
arthurpcardoso <arthurpcardoso13@gmail.com>
2024-08-09 12:31:38 -0300

    inicio

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import { Content } from "next/font/google";
+import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+const config: Config = withMT({
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#235256',
+      },
+    },
+  },
+  plugins: [],
+});
+
+export default config;
